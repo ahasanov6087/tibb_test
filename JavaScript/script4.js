0b40aaabbf4ef57e9a3ca62751534902f91d705f
ahasanov6087 <ahasanov6087@ada.edu.az>
2026-01-29 23:39:56 +0400

    mal sehvlik

--- JavaScript/script4.js
+++ JavaScript/script4.js
@@ -7,7 +7,7 @@ if (!localStorage.getItem('users')) {
         { id: 2, email: "admin@example.com", password: "admin456" },
         { id: 3, email: "intaynur@mail.ru", password: "hasanova" },
         { id: 4, email: "lemanamrahova@mail.ru", password: "amrahova" },
-        { id: 5, email: "revanibrahimli121618@gmail.com", password: "ruhiyye0101" }
+        { id: 5, email: "revanibrahimli121618@gmail.com", password: "ruhiyye0101" },
         { id: 6, email: "anarismoyilov791@gmail.com", password: "1981" },
     ]));
 }
